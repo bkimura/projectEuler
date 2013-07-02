d45f02b4ecc97554917c0c048373108cc972b8d4
BKimura <brennan.kimura@gmail.com>
2013-07-01 23:11:32 -0600

    Some minor adjustments

--- problem001/problem001.cpp
+++ problem001/problem001.cpp
@@ -8,19 +8,21 @@ using namespace std;
 
 int main() {
 
-int sum = 0;
+  int sum = 0;
+  
+  for(int i = 3; i < 1000; i+=3) {
+    sum+=i;
+  }
+  
+  for(int i = 5; i < 1000; i+=5) {
+    sum+=i;
+  }
+  
+  for(int i = 15; i < 1000; i+=15) {
+    sum-=i;
+  }
+  
+  cout << "Solution: " << sum << endl;
 
-for(int i = 3; i < 1000; i+=3) {
-  sum+=i;
-}
-
-for(int i = 5; i < 1000; i+=5) {
-  sum+=i;
-}
-
-for(int i = 15; i < 1000; i+=15) {
-  sum-=i;
-}
-
-cout << "Solution: " << sum << endl;
+  return 0;
 }
--- problem002/problem002.cpp
+++ problem002/problem002.cpp
@@ -27,4 +27,5 @@ int main() {
 
   cout << "Solution: " << sum << endl;
 
+  return 0;
 }
