233fdf76de230335f1eef5e5ec1a3fb43f5731a8
BKimura <brennan.kimura@gmail.com>
2013-07-09 17:13:45 -0600

    My first attempt at a palidrome function... FAIL

---
+++ problem004/problem004.cpp
@@ -0,0 +1,41 @@
+// Find the largest palindrome made
+// from the product of two 3-digit
+// numbers.
+
+#include <iostream>
+
+using namespace std;
+
+bool isPalidrome(int num) {
+  int temp = 0, rev = 0, add = 0;
+  while(temp > 0) {
+    add = temp % 10;
+    rev += add;
+    rev *= 10;
+    add /= 10;
+  }
+  if(num = rev) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+int main() {
+  
+  int test = 1001;
+  int fail = 11001;
+
+  if(isPalidrome(test)) {
+    cout << test << " is a palindrome." << endl;
+  } else {
+    cout << test << " is NOT a palindrome." << endl;
+  }
+  if(isPalidrome(fail)) {
+    cout << fail << " is a palindrome." << endl;
+  } else {
+    cout << fail << " is NOT a palindrome." << endl;
+  }
+
+  return 0;
+}
