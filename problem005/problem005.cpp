df268be54962858ae6441557f523829b2ca0ec1c
BKimura <brennan.kimura@gmail.com>
2013-07-14 22:56:03 -0600

    Figured it out, also I need to commit more frequently when writing my code. Runs quicker than I thought.

--- problem005/problem005.cpp
+++ problem005/problem005.cpp
@@ -8,6 +8,26 @@ using namespace std;
 
 int main() {
 
+  int mult = 2;
+  bool foundLCM = false;
+  int test = 20;
+
+  while(!foundLCM) {
+
+    test = 20 * mult;
+    for(int i = 19; i > 0; i--) {
+      if(test%i != 0) {
+        break;
+      }
+      if(i == 2) {
+        foundLCM = true;
+      }
+    }    
+    mult++;
+  }
+
+  cout << "Solution: " << test << endl;
+
   return 0;
 
 }
