4354e788582976106f2e52eb02e97e0559f615e3
BKimura <brennan.kimura@gmail.com>
2013-07-15 21:53:25 -0600

    Starting problem 6.

---
+++ problem006/problem006.cpp
@@ -0,0 +1,13 @@
+// Problem: Find the difference between the sum of the
+// squares of the first one hundred natural numbers
+// and the square of the sum.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+  return 0;
+
+}
