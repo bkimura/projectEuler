a1b63cce8db292095f7ca5f1675e4ba2ea0e2488
BKimura <brennan.kimura@gmail.com>
2013-07-19 23:11:47 -0600

    I am working and committing. I love code on a friday night.

--- problem006/problem006.cpp
+++ problem006/problem006.cpp
@@ -6,7 +6,29 @@
 
 using namespace std;
 
+int sumOfSquare(int beg, int end) {
+  
+  int max = 0;
+  int 
+
+  return temp;
+}
+
+int squareOfSum(int beg, int end) {
+  
+  int n = end - beg + 1;
+  int temp = n*(beg + end)/2;
+
+  temp *= temp;
+
+  return temp;
+}
+
 int main() {
+
+  cout << "Solution: " << squareOfSum(1, 5) << endl;
+  cout << "Solution: " << squareOfSum(3, 5) << endl;
+  cout << "Solution: " << squareOfSum(1, 10) << endl;
   
   return 0;
 
