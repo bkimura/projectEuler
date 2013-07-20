fce6c604d63f57b050c597a6f2eade5721b0c31e
BKimura <brennan.kimura@gmail.com>
2013-07-19 23:49:44 -0600

    Finished two test cases, also got the answer. Hey oh

--- problem006/problem006.cpp
+++ problem006/problem006.cpp
@@ -8,10 +8,10 @@ using namespace std;
 
 int sumOfSquare(int beg, int end) {
   
-  int max = 0;
-  int 
+  int max = end*(2*end+1)*(end+1)/6;
+  int min = (beg-1)*(2*(beg-1)+1)*((beg-1)+1)/6;
 
-  return temp;
+  return max-min;
 }
 
 int squareOfSum(int beg, int end) {
@@ -26,10 +26,14 @@ int squareOfSum(int beg, int end) {
 
 int main() {
 
-  cout << "Solution: " << squareOfSum(1, 5) << endl;
-  cout << "Solution: " << squareOfSum(3, 5) << endl;
-  cout << "Solution: " << squareOfSum(1, 10) << endl;
-  
+  cout << "Beg = 1, End 10, " << "Test Case 1: " << squareOfSum(1, 10) << endl;
+  cout << "Beg = 1, End 10, " << "Test Case 1: " << sumOfSquare(1, 10) << endl << endl;
+
+  cout << "Beg = 3, End 5, " << "Test Case 2: " << squareOfSum(3, 5) <<  endl;
+  cout << "Beg = 3, End 5, " << "Test Case 2: " << sumOfSquare(3, 5) <<  endl << endl;
+
+  cout << "Solution: " << squareOfSum(1, 100) - sumOfSquare(1, 100) << endl;
+
   return 0;
 
 }
