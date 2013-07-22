c99ba067f34f467e8e5040885cf010c8d6283f1c
BKimura <brennan.kimura@gmail.com>
2013-07-21 22:02:59 -0600

    This is a quick attack at the problem. Now it's time to test.

--- problem007/problem007.cpp
+++ problem007/problem007.cpp
@@ -6,6 +6,24 @@ using namespace std;
 
 int main() {
   
+  int n = 0, num = 1;
+
+  while(n > 100001) {
+    num++;
+    for(int i = 2; i < num; i++) {
+
+      if((i%num) == 0) {
+        break;
+      } else if(i == (num - 1)) {
+        n++;
+      }
+
+    }
+
+  }
+
+  cout << "Solution: " << num << endl;
+  
   return 0;
 
 }
