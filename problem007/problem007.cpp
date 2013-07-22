fc8fb3e1cd8898f109a5330abea15eca688a59ad
BKimura <brennan.kimura@gmail.com>
2013-07-21 22:25:55 -0600

    Debugging, so much fun... Back to the pen and paper.

--- problem007/problem007.cpp
+++ problem007/problem007.cpp
@@ -6,15 +6,20 @@ using namespace std;
 
 int main() {
   
-  int n = 0, num = 1;
+  // n = number of primes found
+  // num = number being tested
+  int n = 1, num = 1;
 
-  while(n > 100001) {
+  // loop until the 10001st prime 
+  // is found
+  while(n < 10001) {
     num++;
     for(int i = 2; i < num; i++) {
 
       if((i%num) == 0) {
         break;
       } else if(i == (num - 1)) {
+        cout << "Prime found: " << num << endl;
         n++;
       }
 
